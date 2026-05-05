253f3beda6eac978f6849123d6b1e11054fd5df4
Aresquake <ares080429@gmail.com>
2026-05-06 00:54:35 +0800

    server.ts修正

--- server.ts
+++ server.ts
@@ -54,9 +54,9 @@ async function startServer() {
         console.error("Vite server creation failed:", viteErr);
       }
     } else {
-      app.use(express.static(path.join(__dirname, "dist")));
+      app.use(express.static(path.join(__dirname, ".")));
       app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "dist", "index.html"));
+        res.sendFile(path.join(__dirname, "index.html"));
       });
     }
 
